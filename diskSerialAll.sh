4203c2d7dad53ee5572462b60a5e257c10b77936
HackDL <31467324+HackDL@users.noreply.github.com>
2022-01-14 23:10:38 +0800

    Update diskSerialAll.sh

--- diskSerialAll.sh
+++ diskSerialAll.sh
@@ -1,1 +1,1 @@
-for num in `lsscsi | grep /dev/sd. | awk '{print $6}'`;do echo $num;echo `sudo smartctl -a $num | grep Serial`; done;
+for num in `lsscsi | grep /dev/sd. | awk '{print $6}'| sort | grep ^/`;do echo $num;echo `sudo smartctl -a $num | grep -E "Serial|Device Model|Product"`; done;
